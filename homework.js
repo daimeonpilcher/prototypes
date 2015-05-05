fe43d6287aad4c0567e7522919ed1a772129c29e
DAIMEON PILCHER <daimeon.pilcher@gmail.com>
2015-05-05 09:13:59 -0700

    Week 2 Day 1 HW

---
+++ homework.js
@@ -0,0 +1,18 @@
+// dice constructor
+function Dice(desc, sides) {
+	this.desc = desc;
+	this.sides = sides;
+}
+
+var lastRoll = {
+	this.desc = desc
+	this.rolls = 0
+	this.currRoll = currRoll
+}
+
+Dice.prototype.roll() {
+	lastRoll.rolls += 1;
+	function(){
+		this.currRoll((Math.random()this.sides) + 1) = lastRoll.currRoll;
+	}
+}
